c2b619b1af85886b0e7d8ff9805369d911991c12
Andy Maklakov <88806378+AndyMaklakov@users.noreply.github.com>
2022-08-17 12:29:29 +1200

    Updated version2 with colour scheme 1 and trailing/improvements - home.php

--- home.php
+++ home.php
@@ -1,3 +1,13 @@
+<?php
+        session_start();
+        if(!isset($_SESSION['login_user'])){
+                header("location:login.php");
+				}
+		else{
+			$User = $_SESSION['login_user'];
+        }
+?>
+
 <!DOCTYPE html>
 <html>
    <head>
@@ -14,27 +24,37 @@
          </div>-->
 		  
          <div class = "nav">
+			 
+			 <div class = "logo" style = float:right>
+				
+				<center>
+						
+					<p>logo</p>
+					
+				</center>
+				
+			 </div>
+			 
+			 <div class = "link" style = float:right>
+				 <?php
+				 if ($_SESSION['login_user'] == "graeme" or $_SESSION['login_user'] == "Graeme"){
+					 echo '<a class="query_nav" href="insert_user.php">Settings</a>';
+				 }
+				 ?>
+			 </div>
+			 
             <div class="hamburger-menu">
                <input id="menu__toggle" type="checkbox" />
                <label class="menu__btn" for="menu__toggle">
                <span></span>
                </label>
                <ul class="menu__box">
-                  <li><a class="menu__item" href="home.html">Home</a></li>
-                  <li><a class="menu__item" href="library.html">Library</a></li>
-                  <li><a class="menu__item" href="login.html">Login</a></li>
+                  <li><a class="menu__item" href="home.php">Home</a></li>
+                  <li><a class="menu__item" href="library.php">Library</a></li>
+                  <li><a class="menu__item" href="logout.php">Logout</a></li>
                </ul>
             </div>
-			 
-            <div class = "logo">
-				
-               <p>logo</p>
-				
-			 </div>
-            
-			 
-			 <p>logo</p>
-			 
+ 
          </div>
          <div class = "main_content">
             
