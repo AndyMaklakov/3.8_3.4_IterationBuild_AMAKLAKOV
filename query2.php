4ccb4c118de8a3328f60a0def7a6c845930c7c54
Andy Maklakov <88806378+AndyMaklakov@users.noreply.github.com>
2022-09-04 20:27:27 +1200

    Updated query2.php by adding images/graphics and improvements

--- query2.php
+++ query2.php
@@ -15,6 +15,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<link rel="stylesheet" href="css/style.css">
+		<link rel="icon" type="image/x-icon" href="/images/sigma_favicon.ico">
 		<title>Query 2</title>
 	</head>
 
@@ -28,14 +29,7 @@
 			
 			<div class = "nav">
 				
-				<div class = "logo" style = float:right>
-				
-					<center>
-						
-						<p>logo</p>
-					
-					</center>
-				
+				<div class = "logo" style = "background-image: url('images/sigmalogowhite.jpg'); background-size: 4vw 4vw; background-position: 0vw;" alt="sigmalogowhite">
 			 	</div>
 				
 				<div class = "admin_nav" style = float:right>
@@ -72,55 +66,54 @@
 				
 
 				
-				<div class = "title2">
+				<div class = "title2" style = "background-image: url('images/query2bannerimage1.jpg'); background-position: 0vw;" alt="query2bannerimage1">
+					<center>
 
-						<center>
-							<h1>Query2</h1>
-							<p>Graeme's playlist: sorted by genre, artist ASC</p>
-						</center>
-						
-				</div>
+							<h1 style = "color: white;">Query2</h1>
+							<p style = "color: white;">Graeme's playlist: sorted by genre, artist ASC</p>
+					</center>
 
-				<div class = "title3" style = "border-bottom: 0.1vw solid #000;">
-					
-							<?php
-								require "91902_Database_Assessment_mysqli.php";
+				
+					<div class = "title3" style = "0.1vw solid #000; margin-top: 3.45vw;" alt="librarybannerimage1">
+								<?php
+									require "91902_Database_Assessment_mysqli.php";
 
-								//Creates a variable to store the sql query
-								$query = ("SELECT SEC_TO_TIME(SUM(Seconds))
-											FROM song_details
-											WHERE 1");
+									//Creates a variable to store the sql query
+									$query = ("SELECT SEC_TO_TIME(SUM(Seconds))
+												FROM song_details
+												WHERE 1");
 
-								//Runs and stores the query using the variables $con (see nav.php) and $query
-								$result = mysqli_query($conn,$query);
-								//runs in a while 'while' loop
-								while($output=mysqli_fetch_array($result))
-								{
-							?>
-					
-							<p style = "padding-left: 21.3vw; display: inline;">#</p>
+									//Runs and stores the query using the variables $con (see nav.php) and $query
+									$result = mysqli_query($conn,$query);
+									//runs in a while 'while' loop
+									while($output=mysqli_fetch_array($result))
+									{
+								?>
 
-							<p style = "padding-left: 2.5vw; display: inline;">TITLE</p>
+								<p style = "color: white; padding-left: 21.3vw; display: inline;">#</p>
 
-							<p style = "padding-left: 12.8vw; display: inline;">ARTIST</p>
+								<p style = "color: white; padding-left: 2.5vw; display: inline;">TITLE</p>
 
-							<p style = "padding-left: 4vw; display: inline;">ALBUM</p>
+								<p style = "color: white; padding-left: 12.8vw; display: inline;">ARTIST</p>
 
-							<p style = "padding-left: 9.3vw; display: inline;">GENRE</p>
+								<p style = "color: white; padding-left: 4vw; display: inline;">ALBUM</p>
 
-							<p style = "padding-left: 4.2vw; display: inline;">SEC</p>
+								<p style = "color: white; padding-left: 9.3vw; display: inline;">GENRE</p>
 
-							<p style = "padding-left: 2.5vw; display: inline;">SIZE</p>
-							
-							<p style = "padding-left: 6vw; display: inline;">Total Time:</p>
-							
-							<p style = "padding-left: 0.5vw; display: inline;"><?php echo $output['SEC_TO_TIME(SUM(Seconds))']; ?></p>
-								
-							<?php
-							//Closes the output while loop
-							}
-							?>
-					
+								<p style = "color: white; padding-left: 4.2vw; display: inline;">SEC</p>
+
+								<p style = "color: white; padding-left: 2.5vw; display: inline;">SIZE</p>
+
+								<p style = "color: white; padding-left: 6vw; display: inline;">Total Time:</p>
+
+								<p style = "color: white; padding-left: 0.5vw; display: inline;"><?php echo $output['SEC_TO_TIME(SUM(Seconds))']; ?></p>
+
+								<?php
+								//Closes the output while loop
+								}
+								?>
+
+					</div>
 				</div>
 
 
@@ -150,22 +143,22 @@
 								?>
 								<!--php is above. HTML is below. Used to output the query result-->
 						
-								<div class ="test" style = "border-bottom: 0.1vw solid #000; border-left: 0.1vw solid #000; border-right: 0.1vw solid #000;">
+								<div class ="test" style = "border-bottom: 0.1vw solid #FFF; border-left: 0.1vw solid #FFF; border-right: 0.1vw solid #FFF;">
 									<heading2>
 
-										<p><?php echo $output['Song_ID']; ?></p>
+										<p style = "color: white;"><?php echo $output['Song_ID']; ?></p>
 
-										<p><?php echo $output['Title']; ?></p>
+										<p style = "color: white;"><?php echo $output['Title']; ?></p>
 
-										<p><?php echo $output['Artist']; ?></p>
+										<p style = "color: white;"><?php echo $output['Artist']; ?></p>
 
-										<p><?php echo $output['Album']; ?></p>
+										<p style = "color: white;"><?php echo $output['Album']; ?></p>
 
-										<p><?php echo $output['Genre']; ?></p>
+										<p style = "color: white;"><?php echo $output['Genre']; ?></p>
 
-										<p><?php echo $output['Seconds']; ?></p>
+										<p style = "color: white;"><?php echo $output['Seconds']; ?></p>
 
-										<p><?php echo $output['Size']; ?></p>
+										<p style = "color: white;"><?php echo $output['Size']; ?></p>
 
 									</heading2>
 
